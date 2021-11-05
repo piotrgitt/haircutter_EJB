e8498168a0d6322a59b699ac7ea3e740e452086c
Piotr <49452547+piotrgitt@users.noreply.github.com>
2021-11-05 16:21:44 +0100

    dodano filtrowanie

--- ejbModule/jsf/haircutter/dao/ReservationDAO.java
+++ ejbModule/jsf/haircutter/dao/ReservationDAO.java
@@ -9,6 +9,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import jsf.haircutter.entities.Reservation;
+import jsf.haircutter.entities.User;
 
 
 @Stateless
@@ -35,11 +36,52 @@ public class ReservationDAO {
 		return em.find(Reservation.class, id);
 	}
 	
+	public List<Reservation> getList(Map<String, Object> searchParams) {
+		List<Reservation> list = null;
+
+		// 1. Build query string with parameters
+		String select = "select r ";
+		String from = "from Reservation r ";
+		String where = "";
+		String orderby = "order by r.time";
+
+		// search for surname
+		String name = (String) searchParams.get("name");
+		if (name != null) {
+			if (where.isEmpty()) {
+				where = "where ";
+			} else {
+				where += "and ";
+			}
+			where += "u.name like :name ";
+		}
+		
+		// ... other parameters ... 
+
+		// 2. Create query object
+		Query query = em.createQuery(select + from + where + orderby);
+
+		// 3. Set configured parameters
+		if (name != null) {
+			query.setParameter("name", name+"%");
+		}
+
+		// ... other parameters ... 
+
+		// 4. Execute query and retrieve list of Person objects
+		try {
+			list = query.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
 	
 	public List<Reservation> getFullList() {
 		List<Reservation> list = null;
 
-		Query query = em.createQuery("select r from Reservation r");
+		Query query = em.createQuery("select r from Reservation r order by r.time DESC");
 
 		try {
 			list = query.getResultList();
